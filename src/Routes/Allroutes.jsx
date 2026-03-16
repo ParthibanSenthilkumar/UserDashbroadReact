30c8278a8afebc24161c20ad8203f09b5e01c8d3
Parthiban <parthiban.dev06@gmail.com>
2026-03-16 15:25:52 +0530

    file named

--- src/Routes/Allroutes.jsx
+++ src/Routes/Allroutes.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Route, Routes } from 'react-router-dom'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
-import Dashbroad from '../pages/Dashbroad'
+import Dashbroad from '../pages/Dashboard'
 
 const Allroutes = () => {
   return (
@@ -11,7 +11,7 @@ const Allroutes = () => {
         < Route path='/' element={ <Login /> } /> 
         < Route path='/register' element={ <Register /> } />
         < Route path='/login' element={ <Login /> } />  
-        <Route path='/dashbroad' element={ <Dashbroad /> } />
+        <Route path='/Dashboard' element={ <Dashboard /> } />
       </Routes>
     </>
   )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,79 +13,83 @@ const Register = () => {
 
   let handleSubmit = async (e) => {
     e.preventDefault();
-    try{
-        setloading(true);
-    let registerFunc = await createUserWithEmailAndPassword(
+    try {
+      setloading(true);
+      let registerFunc = await createUserWithEmailAndPassword(
         auth,
         useremail,
         userpass,
-        );
-        console.log("authen Data", registerFunc);
+      );
+      console.log("authen Data", registerFunc);
 
-        await fetch(
+      await fetch(
         "https://usermangement-19026-default-rtdb.firebaseio.com/useregister.json",
         {
-            method: "post",
-            headers: {
+          method: "post",
+          headers: {
             "content-type": "application/json",
-            },
-            body: JSON.stringify({
+          },
+          body: JSON.stringify({
             user: user,
             userpass: userpass,
             useremail: useremail,
             createdAt: new Date().toISOString(),
-            }),
+          }),
         },
-        useremail(''),
-        userpass(''),
-        user(''),
-        successToast("Register Successfull")
-        );
-    }
-    catch(error){
-        errorToast(error.message)
-    }
-    finally{
-        setloading(false);
+        useremail(""),
+        userpass(""),
+        user(""),
+        successToast("Register Successfull"),
+      );
+    } catch (error) {
+      errorToast(error.message);
+    } finally {
+      setloading(false);
     }
   };
   return (
     <>
-    {loading ? ( <div className="d-flex align-items-center justify-content-center vh-100 vw-100"><Loader /></div>) :(
-    <div className="form-group">
-      <form onSubmit={handleSubmit}>
-          <div className="form-item">
-            <label>Name </label>
-            <input
-              type="text"
-              value={user}
-              placeholder="enter the Email"
-              onChange={(e) => setuser(e.target.value)}
-            />
-          </div>
-          <div className="form-item">
-            <label>Email </label>
-            <input
-              type="email"
-              value={useremail}
-              placeholder="enter the Email"
-              onChange={(e) => setemail(e.target.value)}
-            />
-          </div>
-          <div className="form-item">
-            <label>Password </label>
-            <input
-              type="password"
-              value={userpass}
-              placeholder="enter the password"
-              onChange={(e) => setpass(e.target.value)}
-            />
-          </div>
-        <button className="btn-gradient">Register</button>
-        <h5 className="footer_text">You have an account? <Link to="/login">Login</Link></h5>
-      </form>
-       </div>
-    )}
+      {loading ? (
+        <div className="d-flex align-items-center justify-content-center vh-100 vw-100">
+          <Loader />
+        </div>
+      ) : (
+        <div className="form-group">
+          <form onSubmit={handleSubmit}>
+            <div className="form-item">
+              <label>Name </label>
+              <input
+                type="text"
+                value={user}
+                placeholder="enter the Email"
+                onChange={(e) => setuser(e.target.value)}
+              />
+            </div>
+            <div className="form-item">
+              <label>Email </label>
+              <input
+                type="email"
+                value={useremail}
+                placeholder="enter the Email"
+                onChange={(e) => setemail(e.target.value)}
+              />
+            </div>
+            <div className="form-item">
+              <label>Password </label>
+              <input
+                type="password"
+                value={userpass}
+                placeholder="enter the password"
+                onChange={(e) => setpass(e.target.value)}
+              />
+            </div>
+            <button className="btn-gradient">Register</button>
+            <h5 className="footer_text">
+              You have an account? <Link to="/login">Login</Link>
+            </h5>
+          </form>
+        </div>
+      )}
     </>
   );
 };
